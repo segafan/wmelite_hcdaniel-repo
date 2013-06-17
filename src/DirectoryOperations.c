78557102ef86d32f74a3de2e3a0a4f20278d4a11
hardcoredaniel <hardcoredaniel@localhost>
2013-06-17 20:57:27 +0200

    implement plain file and directory operations, probably not yet platform safe

--- src/DirectoryOperations.c
+++ src/DirectoryOperations.c
@@ -6,13 +6,46 @@
  */
 
 #include <stdlib.h>
+#include <sys/types.h>
+#include <dirent.h>
 
 #include "DirectoryOperations.h"
 
+static DIRHANDLE  dir_open_plain(const char *path);
+static char     *dir_read_plain(DIRHANDLE handle);
+static int        dir_close_plain(DIRHANDLE handle);
+static char     *dir_get_package_extension_plain(void);
+
+generic_directory_ops directory_ops_plain =
+{
+	.dir_open                  = dir_open_plain,
+	.dir_read                  = dir_read_plain,
+	.dir_close                 = dir_close_plain,
+	.dir_get_package_extension = dir_get_package_extension_plain
+};
+
 generic_directory_ops *get_directory_operations(dir_access_variant access_variant)
 {
-	return NULL;
+	return &directory_ops_plain;
 }
 
+static DIRHANDLE  dir_open_plain(const char *path)
+{
+	return (DIRHANDLE) opendir(path);
+}
 
+static char     *dir_read_plain(DIRHANDLE handle)
+{
+	struct dirent* ent = readdir((DIR*) handle);
+	return ent->d_name;
+}
 
+static int        dir_close_plain(DIRHANDLE handle)
+{
+	return closedir((DIR*) handle);
+}
+
+static char     *dir_get_package_extension_plain(void)
+{
+	return "dcp";
+}
--- src/FileOperations.c
+++ src/FileOperations.c
@@ -6,11 +6,67 @@
  */
 
 #include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
 
 #include "FileOperations.h"
 
+static int        file_exists_plain(const char *name);
+static FILEHANDLE file_open_plain(const char *name, const char *mode);
+static long       file_read_plain(char *buffer, long size, FILEHANDLE handle);
+static long       file_write_plain(const char *buffer, long size, FILEHANDLE handle);
+static int        file_seek_plain(FILEHANDLE handle, long offset, int whence);
+static long       file_tell_plain(FILEHANDLE handle);
+static int        file_close_plain(FILEHANDLE handle);
+
+
+generic_file_ops file_ops_plain =
+{
+	.file_exists = file_exists_plain,
+	.file_open   = file_open_plain,
+	.file_read   = file_read_plain,
+	.file_write  = file_write_plain,
+	.file_seek   = file_seek_plain,
+	.file_tell   = file_tell_plain,
+	.file_close  = file_close_plain
+};
+
 generic_file_ops *get_file_operations(file_access_variant access_variant)
 {
-	return NULL;
+	return &file_ops_plain;
 }
 
+static int        file_exists_plain(const char *name)
+{
+	return access(name, R_OK);
+}
+
+static FILEHANDLE file_open_plain(const char *name, const char *mode)
+{
+	return (FILEHANDLE) fopen(name, mode);
+}
+
+static long       file_read_plain(char *buffer, long size, FILEHANDLE handle)
+{
+	return fread(buffer, size, 1, (FILE*) handle);
+}
+
+static long       file_write_plain(const char *buffer, long size, FILEHANDLE handle)
+{
+	return fwrite(buffer, size, 1, (FILE*) handle);
+}
+
+static int        file_seek_plain(FILEHANDLE handle, long offset, int whence)
+{
+	return fseek((FILE*) handle, offset, whence);
+}
+
+static long       file_tell_plain(FILEHANDLE handle)
+{
+	return ftell((FILE*) handle);
+}
+
+static int        file_close_plain(FILEHANDLE handle)
+{
+	return fclose((FILE*) handle);
+}
