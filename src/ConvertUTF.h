776b00b99f522a823df598f707cf7f7a4ec278fc
hardcoredaniel <hardcoredaniel@localhost>
2014-04-18 15:20:55 +0200

    another 64 bit linux crash fix

--- src/ConvertUTF.h
+++ src/ConvertUTF.h
@@ -87,7 +87,13 @@
     bit mask & shift operations.
 ------------------------------------------------------------------------ */
 
-typedef unsigned long	UTF32;	/* at least 32 bits */
+#ifdef __WIN32__
+#define uint32_t unsigned long
+#else
+#include <stdint.h>
+#endif
+
+typedef uint32_t        UTF32;  /* at least 32 bits */
 typedef unsigned short	UTF16;	/* at least 16 bits */
 typedef unsigned char	UTF8;	/* typically 8 bits */
 typedef unsigned char	Boolean; /* 0 or 1 */
