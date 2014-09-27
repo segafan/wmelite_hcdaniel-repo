4988a68c5f1482ed5482ec50d120d712e2909b35
hardcoredaniel <hardcoredaniel@localhost>
2014-09-27 15:53:50 +0200

    update version numbers

--- src/dcgf.h
+++ src/dcgf.h
@@ -30,8 +30,8 @@ THE SOFTWARE.
 //////////////////////////////////////////////////////////////////////////
 #define DCGF_VER_MAJOR 1
 #define DCGF_VER_MINOR 0
-#define DCGF_VER_BUILD 2
-#define DCGF_VER_SUFFIX "beta"
+#define DCGF_VER_BUILD 6
+#define DCGF_VER_SUFFIX "rapaki"
 #define DCGF_VER_BETA true
 
 #define DCGF_NAME "WME Lite"
